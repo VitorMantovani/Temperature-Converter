fece04ecad726027921e644475daf6f7bcf394a1
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-01-19 14:50:38 -0300

    feat: creates execute() method

--- src/use-cases/TemperatureConverter.ts
+++ src/use-cases/TemperatureConverter.ts
@@ -1,3 +1,11 @@
+type dataToConvert = {
+    temperature: number,
+    scale: string,
+    scaleToConvert: string,
+}
+
 export class TemperatureConverter {
-    
+    static execute({temperature, scale, scaleToConvert}: dataToConvert) {
+
+    }
 }
