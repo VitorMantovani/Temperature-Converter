baef339c9e1e6140f05bbf216b514fe163cb5d42
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-01-19 15:20:52 -0300

    feat: adds celsius to farenheit conversion

--- src/use-cases/TemperatureConverter.ts
+++ src/use-cases/TemperatureConverter.ts
@@ -1,11 +1,20 @@
 type dataToConvert = {
-    temperature: number,
-    scale: string,
-    scaleToConvert: string,
-}
+  temperature: number;
+  scale: string;
+  scaleToConvert: string;
+};
+
+type convertedData = Record<string, number>;
 
 export class TemperatureConverter {
-    static execute({temperature, scale, scaleToConvert}: dataToConvert) {
-
+  static execute({
+    temperature,
+    scale,
+    scaleToConvert,
+  }: dataToConvert) {
+    if (scale === "celsius" && scaleToConvert === "fahrenheit") {
+      const fahrenheitTemperature = temperature * 1.8 + 32;
+      return { "celsius": temperature, "fahrenheit":  fahrenheitTemperature };
     }
+  }
 }
