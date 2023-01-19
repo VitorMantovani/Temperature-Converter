6abce53fac621933729839432a88c9b74b9734c4
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-01-19 15:35:12 -0300

    feat: adds celsius to kelvin conversion

--- src/use-cases/TemperatureConverter.ts
+++ src/use-cases/TemperatureConverter.ts
@@ -21,5 +21,10 @@ export class TemperatureConverter {
         const celsiusTemperature = (temperature - 32) / 1.8;
         return { "fahrenheit": temperature, "celsius":  celsiusTemperature };
       }
+
+      if (scale === "celsius" && scaleToConvert === "kelvin") {
+        const kelvinTemperature = temperature + 273.15;
+        return { "celsius": temperature, "kelvin":  kelvinTemperature};
+      }
   }
 }
