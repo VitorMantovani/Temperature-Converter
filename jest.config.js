02fd3c6f1de6db146723bcc66845b754e425d12f
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-01-20 14:09:20 -0300

    config: adds diagnostics: false to jest file

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  global: {
+    "ts-jest": {
+      diagnostics: false,
+    },
+  },
 };
